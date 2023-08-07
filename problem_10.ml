fafa294fbbceb3ebda9bf44474066f1f9cc14bcb
Gethin Davies <gethin.davies@motilent.co.uk>
2023-08-07 22:48:36 +0100

    Problem 10
    Fix partial match (after getting help from the answer)

--- problem_10.ml
+++ problem_10.ml
@@ -1,9 +1,11 @@
 (* run length encoding *)
-let rec rle (h::t) =
-  let rec aux acc n e l = match l with
-  | [] -> (n, e) :: acc
-  | x :: xs -> if x = e then aux acc (n + 1) e xs else aux ((n, e)::acc) 1 x xs
-in aux [] 1 h t |> List.rev
+let rec rle list =
+  let rec aux n acc = function
+  | [] -> []
+  | [x] -> (n+1, x) :: acc
+  | a :: (b :: _ as t) -> if a = b then aux (n + 1) acc t
+                          else aux 0 ((n + 1, a) :: acc) t
+                        in List.rev (aux 0 [] list);;
 
 (* --- *)
 
@@ -16,6 +18,4 @@ let list_to_str = function
 
 let xs = ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"];;
 
-let main () = rle xs |> List.map pp_tuple |> list_to_str |> print_endline;;
-
-main
+let () = rle xs |> List.map pp_tuple |> list_to_str |> print_endline;;
