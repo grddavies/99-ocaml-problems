f5832b488de0e02465d9cb4bbe0ae39838873407
Gethin Davies <gethin.davies@motilent.co.uk>
2023-08-08 21:41:39 +0100

    Problem 17
    Hand-rolled recursion

--- problem_17.ml
+++ problem_17.ml
@@ -15,3 +15,11 @@ let take n l =
   List.rev (aux [] n l)
 
 let split list n = (take n list, drop n list)
+
+let split_2 list n =
+  let rec aux acc i = function
+    | [] -> (List.rev acc, [])
+    | x :: xs as rest ->
+        if i = 0 then (List.rev acc, rest) else aux (x :: acc) (i - 1) xs
+  in
+  aux [] n list
