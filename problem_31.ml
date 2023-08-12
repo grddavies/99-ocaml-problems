e502153f159536d9eb8a8521c694ad44b6ccc75e
Gethin Davies <grd.davies@gmail.com>
2023-08-12 16:02:05 +0100

    Use wheel factorization

--- problem_31.ml
+++ problem_31.ml
@@ -1,7 +1,16 @@
 (* Determine whether a given number is prime *)
 let prime n =
-  let lim = n |> float_of_int |> sqrt |> int_of_float in
-  let rec seive i =
-    i > lim || (n mod i <> 0 && seive (i+1))
-  in n <> 1 && seive 2
+  let basis = [2; 3; 5] in
+  let prod = List.fold_left ( * ) 1  basis in
+  let range = List.init prod (fun x -> x + 1) in
+  let wheel = List.filter (fun i -> List.for_all (fun y -> i mod y <> 0) basis) range in
+  let wheel = List.tl wheel in
+  let rec loop base = function
+    (* No coprimes left in this turn of the wheel -> increment base *)
+    | [] -> loop (base + prod) wheel
+    | h :: t ->
+        let i = base + h in
+        i * i > n || n mod i <> 0 && loop base t
+  in n > 1 && loop 0 (basis @ wheel)
+
 
