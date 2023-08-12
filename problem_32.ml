3ced82db40e74fb014a774a4834503b78ed868fa
Gethin Davies <grd.davies@gmail.com>
2023-08-12 20:25:23 +0100

    Use quotient form

--- problem_32.ml
+++ problem_32.ml
@@ -1,6 +1,5 @@
  (* Determine the greatest common divisor of two positive integer numbers. *)
 let rec gcd x y =
-  if x = y then x
-  else if x > y then gcd y (x - y)
-  else gcd x (y - x)
+  if y = 0 then x
+  else gcd y (x mod y)
 
--- problem_33.ml
+++ problem_33.ml
@@ -1,5 +1,6 @@
 (* Determine Whether Two Positive Integer Numbers Are Coprime *)
 let coprime x y =
-  let rec gcd x y = if x = y then x else
-      if x > y then gcd y (x - y) else gcd x (y - x)
+  let rec gcd x y =
+    if y = 0 then x
+    else gcd y (x mod y)
   in gcd x y = 1;
