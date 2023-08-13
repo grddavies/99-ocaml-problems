5e3d11b04c458c32f6f962d3fd4941ead583ceef
Gethin Davies <grd.davies@gmail.com>
2023-08-13 20:47:23 +0100

    Improve space efficiency

--- problem_34.ml
+++ problem_34.ml
@@ -1,8 +1,12 @@
 (* Euler's Totient Function *)
 let phi n =
+  let int_of_bool x = if x then 1 else 0 in
   let rec gcd x y =
     if y = 0 then x
     else gcd y (x mod y) in
   let coprime x y = gcd x y = 1 in
-  let xs = List.init (n - 1) ((+) 1) in
-  if n = 1 then 1 else (List.filter (coprime n) xs) |> List.length
+  let rec count_coprimes acc i = 
+    if i = n then acc
+    else count_coprimes (acc + int_of_bool (coprime i n)) (i + 1) in
+  if n = 1 then 1 else count_coprimes 0 1
+
