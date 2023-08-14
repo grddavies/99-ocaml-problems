b30665d7866d99cb4f3dd830c69a64c008132257
Gethin Davies <grd.davies@gmail.com>
2023-08-14 16:06:07 +0100

    Use wheel factorization

--- problem_35.ml
+++ problem_35.ml
@@ -1,8 +1,17 @@
 (* Determine the Prime Factors of a Given Positive Integer  *)
 let factors n =
-  let rec aux acc d x =
-    if x = 1 then acc
-    else if x mod d = 0 then aux (d::acc) (d) (x/d)
-    else aux acc (d+1) x
-  in aux [] 2 n |> List.rev;;
-
+  let basis = [2; 3; 5] in
+  let prod = List.fold_left ( * ) 1  basis in
+  let range = List.init prod (fun x -> x + 1) in
+  let wheel = List.filter (fun i -> List.for_all (fun y -> i mod y <> 0) basis) range in
+  let wheel = List.tl wheel @ [1+prod] in
+  let rec loop acc x base = function
+    (* No coprimes left in this turn of the wheel -> increment base *)
+    | [] -> loop acc x (base + prod) wheel
+    | h :: t ->
+        let i = base + h in
+        if x = 1 then acc
+        else if x mod i = 0 then loop (i::acc) (x/i) base (basis@wheel)
+        else loop acc x base t
+  in
+  loop [] n 0 (basis@wheel) |> List.rev;;
